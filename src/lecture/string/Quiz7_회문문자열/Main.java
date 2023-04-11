3d8cd8b33451910b84bd411f91a7c8338ca09de9
hyojeong <khj444453@gmail.com>
2023-04-11 23:29:14 +0900

    [Inflearn] Quiz1-7 Solved

---
+++ src/lecture/string/Quiz7_회문문자열/Main.java
@@ -0,0 +1,23 @@
+package lecture.string.Quiz7_회문문자열;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str){
+        str = str.toUpperCase();
+        int lt = 0;
+        int rt = str.length()-1;
+        while (lt < rt){
+            if(str.charAt(lt++) != str.charAt(rt--)) return "NO";
+        }
+        return "YES";
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.println(T.solution(str));
+    }
+}
