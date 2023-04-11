495b35707bb9110e6cffeee83bec7103729c7e5e
hyojeong <khj444453@gmail.com>
2023-04-11 23:28:03 +0900

    [Inflearn] Quiz1-3 Solved

---
+++ src/lecture/string/Quiz3_문장속단어/Main.java
@@ -0,0 +1,21 @@
+package lecture.string.Quiz3_문장속단어;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str){
+        String answer="";
+        for(String s : str.split(" ")){
+            if(s.length() > answer.length()) answer = s;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
