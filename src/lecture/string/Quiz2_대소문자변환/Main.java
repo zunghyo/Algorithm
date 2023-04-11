565ea3a09e1ced6de0195d7149344a955445c586
hyojeong <khj444453@gmail.com>
2023-04-11 23:27:57 +0900

    [Inflearn] Quiz1-2 Solved

---
+++ src/lecture/string/Quiz2_대소문자변환/Main.java
@@ -0,0 +1,22 @@
+package lecture.string.Quiz2_대소문자변환;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str){
+        String answer="";
+        for (char ch : str.toCharArray()) {
+            if(Character.isUpperCase(ch)) answer += Character.toLowerCase(ch);
+            else answer += Character.toUpperCase(ch);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.println(T.solution(str));
+    }
+}
