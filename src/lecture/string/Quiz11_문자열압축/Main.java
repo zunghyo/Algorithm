7a005ae6387870d255b503a0e98d78e949aa8df9
hyojeong <khj444453@gmail.com>
2023-04-13 00:49:56 +0900

    [Inflearn] Quiz1-11 Solved

---
+++ src/lecture/string/Quiz11_문자열압축/Main.java
@@ -0,0 +1,29 @@
+package lecture.string.Quiz11_문자열압축;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str){
+        String answer = "";
+        int cnt = 1;
+        char temp = str.charAt(0);
+        for (int i = 1; i < str.length(); i++) {
+            if(str.charAt(i) == temp) cnt++;
+            else{
+                answer += temp + String.valueOf(cnt).replace("1","");
+                cnt = 1;
+                temp = str.charAt(i);
+            }
+        }
+        answer += temp + String.valueOf(cnt).replace("1","");
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.println(T.solution(str));
+    }
+}
