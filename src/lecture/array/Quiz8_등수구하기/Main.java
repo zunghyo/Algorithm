dc66bad987a4649d6f20d3fe8a8626810e247493
hyojeong <khj444453@gmail.com>
2023-04-14 01:19:14 +0900

    [Inflearn] Quiz2-8 Solved

---
+++ src/lecture/array/Quiz8_등수구하기/Main.java
@@ -0,0 +1,31 @@
+package lecture.array.Quiz8_등수구하기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int[] solution(int[] arr, int n){
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+            for (int j = 0; j < n; j++) {
+                if(arr[i] < arr[j]) rank++;
+            }
+            answer[i] = rank;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : T.solution(arr, n)) {
+            System.out.print(x+" ");
+        }
+    }
+}
