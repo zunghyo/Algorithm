8766838c14292fe499e776a5cd5a3b7dd538b3c2
hyojeong <khj444453@gmail.com>
2023-04-18 21:47:01 +0900

    [Inflearn] Quiz4-1 Solved

---
+++ src/lecture/hashmapandtreeset/Quiz1_학급회장/Main.java
@@ -0,0 +1,33 @@
+package lecture.hashmapandtreeset.Quiz1_학급회장;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public Character solution(int n, String str){
+        Character answer = ' ';
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if(!map.containsKey(str.charAt(i))) map.put(str.charAt(i), 1);
+            else map.put(str.charAt(i), map.get(str.charAt(i))+1);
+        }
+        int max = Integer.MIN_VALUE;
+        for (char key : map.keySet()) {
+            if(map.get(key) > max){
+                max = map.get(key);
+                answer = key;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String str = in.next();
+        System.out.print(T.solution(n, str));
+    }
+}
