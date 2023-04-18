0fb8302e6bf7a561184ff8613b1aa196c5a85a19
hyojeong <khj444453@gmail.com>
2023-04-18 23:50:51 +0900

    [Inflearn] Quiz4-5 Solved

---
+++ src/lecture/hashmapandtreeset/Quiz5_K번째큰수/Main.java
@@ -0,0 +1,38 @@
+package lecture.hashmapandtreeset.Quiz5_K번째큰수;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+
+    public int solution(int[] arr, int n, int k){
+        int answer = -1;
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int l = j+1; l < n; l++) {
+                    int sum = arr[i] + arr[j] + arr[l];
+                    set.add(sum);
+                }
+            }
+        }
+        int cnt = 1;
+        for (int x: set) {
+            if(cnt++ == k) answer = x;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(T.solution(arr, n, k));
+    }
+}
