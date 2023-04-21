d90d745b3d3d45a0f2b1c4ef294bd2d9bb47f69e
hyojeong <khj444453@gmail.com>
2023-04-21 23:17:41 +0900

    [Inflearn] Quiz7-5 Solved

---
+++ src/lecture/recursive_tree_graph/Quiz5_이진트리순회/Main.java
@@ -0,0 +1,37 @@
+package lecture.recursive_tree_graph.Quiz5_이진트리순회;
+
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node lt, rt;
+    Node(int val){
+        data = val;
+        lt = rt = null;
+    }
+}
+public class Main {
+
+    Node root;
+    public void DFS(Node root){
+        if(root==null) return;
+        else {
+            DFS(root.lt);
+            System.out.print(root.data+" ");
+            DFS(root.rt);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.root = new Node(1);
+        T.root.lt = new Node(2);
+        T.root.rt = new Node(3);
+        T.root.lt.lt = new Node(4);
+        T.root.lt.rt = new Node(5);
+        T.root.rt.lt = new Node(6);
+        T.root.rt.rt = new Node(7);
+        T.DFS(T.root);
+    }
+
+}
