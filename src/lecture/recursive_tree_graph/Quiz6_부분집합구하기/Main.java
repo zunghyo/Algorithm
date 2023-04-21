2b77b33f3b10c24417b47f90bbbc8d1b1765a0db
hyojeong <khj444453@gmail.com>
2023-04-22 00:22:57 +0900

    [Inflearn] Quiz7-6 Solved

---
+++ src/lecture/recursive_tree_graph/Quiz6_부분집합구하기/Main.java
@@ -0,0 +1,29 @@
+package lecture.recursive_tree_graph.Quiz6_부분집합구하기;
+
+public class Main {
+
+    static int n ;
+    static int[] ch;
+    public void DFS(int L){
+        String tmp = "";
+        if(L > n){
+            for (int i = 1; i <= n; i++) {
+                if(ch[i] == 1) tmp += i;
+            }
+            if(tmp.length() > 0) System.out.println(tmp);
+        } else{
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        n = 3;
+        ch = new int[n+1];
+        T.DFS(1);
+    }
+
+}
